68837f133495a64d79b45e0e218a10f3b9ac2f89
z4rd0s <christian.boll@inlyse.com>
2024-10-13 01:07:16 +0200

    fix weights path in hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -6,6 +6,6 @@ def objectformer(pretrained=True, **kwargs):
     if pretrained:
         model.load_state_dict(
             torch.hub.load_state_dict_from_url(
-                'https://github.com/wdrink/Objectformer/releases/download/v0.1.0/objectformer.pth')
+                'https://github.com/z4rd0s/objectformer/releases/download/0.1.0/imagenet_pre.pth')
             )
     return 
